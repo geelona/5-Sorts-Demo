befc1b3d0456534b4609653c69ad49221fc3495c
geelona <mr.geelona@gmail.com>
2023-10-11 17:14:23 +0200

    done with a gnome sort function

--- js/sorts-function/gnome-sort-function.js
+++ js/sorts-function/gnome-sort-function.js
@@ -1,3 +1,23 @@
 export function gnomeSortFunction(lines, lineHeightsList) {
-  console.log("gnomeSortFunction");
+  const l = lines.length;
+  let i = 1;
+
+  function doSortStep() {
+    if (i < l) {
+      if (i > 0 && lines[i - 1].clientHeight > lines[i].clientHeight) {
+        let temp = lines[i].clientHeight;
+        lines[i].style.height = lines[i-1].clientHeight + "px";
+        lines[i-1].style.height = temp + "px";
+        i--;
+      } else {
+        i++;
+      }
+
+      if (i < l) {
+        setTimeout(doSortStep, 10 * i);
+      }
+    }
+  }
+
+  doSortStep();
 }
